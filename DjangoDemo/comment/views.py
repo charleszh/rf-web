be7ec1af76cd28bbe548004bbeef07e924d9de00
charleszh <hanqing0611@126.com>
2021-09-03 09:32:26 +0800

    develop the comment submit and display

--- DjangoDemo/comment/views.py
+++ DjangoDemo/comment/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views.generic import TemplateView
+from . import logger
+from .forms import CommentForm
 
-# Create your views here.
+
+class CommentView(TemplateView):
+    logger.info("in comment view")
+    http_method_names = ['post']
+    template_name = 'comment/result.html'
+    logger.info('in comment view')
+    
+    def post(self, request, *args, **kwargs):
+        logger.info('in post')
+        comment_form = CommentForm(request.POST)
+        target = request.POST.get('target')
+        logger.info('in post for comment view')
+        logger.info(comment_form.is_valid())
+        logger.info('test')
+
+        if comment_form.is_valid():
+            logger.info("in is valid")
+            instance = comment_form.save(commit=False)
+            instance.target = target
+            instance.save()
+            succeed = True
+            logger.info(instance)
+            logger.info("the instance")
+            return redirect(target)
+        else:
+            succeed = False
+
+        context = {
+                'succeed': succeed,
+                'form': comment_form,
+                'target': target,
+        }
+        logger.info(context)
+        return self.render_to_response(context)
--- DjangoDemo/newblog/urls.py
+++ DjangoDemo/newblog/urls.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 
 from newblog.views import AuthorView, SearchView, IndexView, CategoryView, TagView, PostListView, PostDetailView, post_list, post_detail
+from config.views import LinkListView
 from config.views import links
+from comment.views import CommentView
 from blogproject.custom_site import custom_site
 
 
@@ -18,5 +20,6 @@ urlpatterns = [
     url(r'^post/(?P<post_id>\d+).html$', PostDetailView.as_view(), name='post-detail'),
     url(r'^search/$', SearchView.as_view(), name='search'),
     url(r'^author/(?P<author_id>\d+)/$', AuthorView.as_view(), name='author'),
-    url(r'^links/$', links),
+    url(r'^links/$', LinkListView.as_view(), name='links'),
+    url(r'^comment/$', CommentView.as_view(), name='comment'),
 ]
